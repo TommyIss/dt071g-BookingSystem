7456c30a9c4d9999e491591d6b27bec1d5035725
Tommy Issa <tommyissa@hotmail.com>
2025-10-25 14:50:07 +0200

    Har skapat klasser för bokningsmodell samt användare

---
+++ BookingSystem/Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace BookingSystem.Models
+{
+    public class ApplicationUser : IdentityUser // Användarklass som ärver från IdentityUser
+    {
+        public ICollection<Booking> Bookings { get; set; } // En lista av bokningar kopplade till användaren
+    }
+}
---
+++ BookingSystem/Models/Booking.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookingSystem.Models
+{
+    public class Booking // Bokningsmodell för tjänster
+    {
+        public int Id { get; set; }
+        [Required]
+        public DateTime TimeSlot { get; set; } // Datum och tid för bokningen
+        [Required]
+        public string ServiceType { get; set; } // Typ av tjänst som bokas
+        public string UserId { get; set; } // ID för användaren som gjort bokningen
+        public ApplicationUser User { get; set; } // Användaren som gjort bokningen
+        public bool IsConfirmed { get; set; } // Bokningsstatus, om den är bekräftad eller inte
+    }
+}
